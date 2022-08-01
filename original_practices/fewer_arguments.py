1be39763a3583162e3b178a0ba08b4f598bea01b
Mikah Mathews <mikah@outofband.org>
2022-08-01 14:51:56 -0700

    :

---
+++ original_practices/fewer_arguments.py
@@ -0,0 +1,6 @@
+from sys import argv
+script, first, second = argv
+
+print("This is the script name: ", script)
+print(f"This is the first variable {first}")
+print(f"This is the second variable {second}")
