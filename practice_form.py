b33dc8af166c5c1d9060c27f591aaeed1d83beeb
Mikah Mathews <mikah@outofband.org>
2022-08-01 14:02:40 -0700

    Add practice_form.py - written to practice inputs and printing

---
+++ practice_form.py
@@ -0,0 +1,17 @@
+print("Let's get some information from you!")
+print("What is your name?")
+name = input()
+
+print("Do you enjoy watching TV?")
+tv = input()
+
+print("Do you like to go hiking?")
+hiking = input()
+
+print("Do you go to protests for civil rights?")
+protests = input()
+
+print(f"Ok, {name}, now here are your answers!")
+print(f"Watching the tv is a {tv} from you.")
+print(f"When asked if you want to go on a hike your answer is {hiking}.")
+print(f"If there is a protest your response in advance is {protests}.")
