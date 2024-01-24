5ce9b6b3d1ba8a9fd35bea77300993238042b424
Mikah Mathews <mikah@outofband.org>
2024-01-24 10:36:50 -0800

    Add exercise 11, exercise 11 study drill, and game.py

---
+++ original_practices/ex11_study_drill.py
@@ -0,0 +1,5 @@
+long_goal = input("What is your long term career goal? ")
+med_goal = input("What is your mid term career goal? " )
+short_goal = input("What is your short term goal? " )
+
+print(f"\nSo your short term goal is {short_goal},\n your mid term goal is {med_goal},\n and your long term goal is {long_goal}")
