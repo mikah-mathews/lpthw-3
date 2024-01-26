011e7edcfdf3a039ed99e8d008f3af42ae5247ff
Mikah Mathews <mikah@outofband.org>
2024-01-26 15:13:49 -0800

    Update file to be in read+write mode, consolidate target.write() to only one line, and add print statement w/updated file info

--- exercises/ex16.py
+++ exercises/ex16.py
@@ -9,9 +9,10 @@ print("If you do want that, hit RETURN.")
 input("?")
 
 print("Opening the file...")
-target = open(filename, 'w')
+target = open(filename, 'r+')
 
 print("Truncating the file. Goodbye!")
+#removes everything from cursor position to end of file if called w/no specifications
 target.truncate()
 
 print("Now I am going to ask you for three lines.")
@@ -22,12 +23,17 @@ line3 = input("line 3: ")
 
 print("I'm going to write these to the file.")
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+# .write() only accepts one variable so this combines all the lines into one
+total_lines = line1 + "\n" + line2 + "\n" + line3 + "\n"
+
+# updates file with 3 lines
+target.write(total_lines)
+
+# after writing a file, the cursor is at the end. to be able to read the file we need to move the cursor to the beginning
+target.seek(0)
+
+print("Now we are going to print the updated file.")
+print(target.read())
 
 print("And finally, we close it.")
 target.close()
