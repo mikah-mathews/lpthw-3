286a01ab3001740e539a5170dffdb4ac183f0caa
Mikah Mathews <mikah@outofband.org>
2024-01-26 09:42:40 -0800

    Update ex13 study drill with input() variable to get more user data

--- original_practices/ex13_study_drill.py
+++ original_practices/ex13_study_drill.py
@@ -2,7 +2,9 @@ from sys import argv
 
 script, name, age, favorite_color = argv
 
-print('Your name is ', name, 'You\'re ', age, 'years old and your favorite color is', favorite_color)
+animal = input("What is your favorite animal? ")
+
+print('Your name is ', name, 'You\'re ', age, 'years old, your favorite color is', favorite_color, 'and your favorite animal is', animal)
 
 # I was confused as to why argv was saying that 'name' was the file name then I realized that the first argument for argv
 # is always going to be the file/script name.
