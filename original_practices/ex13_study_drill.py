99e005a4b02f587ee653e3782c9b25139a464f3a
Mikah Mathews <mikah@outofband.org>
2024-01-26 09:55:18 -0800

    Fix typo in ex13 study drill

--- original_practices/ex13_study_drill.py
+++ original_practices/ex13_study_drill.py
@@ -7,4 +7,4 @@ animal = input("What is your favorite animal? ")
 print('Your name is ', name, 'You\'re ', age, 'years old, your favorite color is', favorite_color, 'and your favorite animal is', animal)
 
 # I was confused as to why argv was saying that 'name' was the file name then I realized that the first argument for argv
-# is always going to be the file/script name.
+# is always going to be the file/script name
