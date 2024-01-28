4bd511888a067ba19950137cfb08780dfa586325
Mikah Mathews <mikah@outofband.org>
2024-01-28 15:12:21 -0800

    Shorten in_file/indata to one line

--- exercises/ex17.py
+++ exercises/ex17.py
@@ -3,11 +3,9 @@ from sys import argv
 script, from_file, to_file = argv
 
 # we could do these two on one line, how?
-in_file = open(from_file)
-indata = in_file.read()
+indata = open(from_file).read()
 
-out_file = open(to_file, 'w')
-out_file.write(indata)
+out_file = open(to_file, 'w').write(indata)
 
 out_file.close()
 in_file.close()
