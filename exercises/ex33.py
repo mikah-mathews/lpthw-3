b8610b30cff170966707211f291ea1e3618c4bf4
Mikah Mathews <mikah@outofband.org>
2024-05-12 19:29:20 -0700

    Update ex33 to be a function and count to number from input

--- exercises/ex33.py
+++ exercises/ex33.py
@@ -1,15 +1,21 @@
-i = 0
-numbers = []
+from sys import argv
+script, input_number = argv
 
-while i < 6:
-    print(f"At the top i is {i}")
-    numbers.append(i)
+def print_to_x(x):
+    i = 0
+    numbers = []
+    
+    while i < x:
+        print(f"At the top i is {i}")
+        numbers.append(i)
 
-    i = i + 1
-    print("Numbers now: ", numbers)
-    print(f"At the bottom i is {i}")
+        i = i + 1
+        print("Numbers now: ", numbers)
+        print(f"At the bottom i is {i}")
 
-print("The numbers: ")
+    print("The numbers: ")
 
-for num in numbers:
-    print(num)
+    for num in numbers:
+        print(num)
+
+print_to_x(int(input_number))
