c2b247bb14767e00c0e9c7ea351543580524da02
Mikah Mathews <mikah@outofband.org>
2024-07-13 16:01:56 -0700

    Update with for-loop

--- exercises/ex33.py
+++ exercises/ex33.py
@@ -1,7 +1,7 @@
 from sys import argv
-script, input_number = argv
+script, count_to, increment_by = argv
 
-def print_to_x(x):
+def print_to_x(x, y):
     i = 0
     numbers = []
     
@@ -9,13 +9,14 @@ def print_to_x(x):
         print(f"At the top i is {i}")
         numbers.append(i)
 
-        i = i + 1
+        i = i + y
         print("Numbers now: ", numbers)
         print(f"At the bottom i is {i}")
 
     print("The numbers: ")
 
+    # create for-loop to print numbers
     for num in numbers:
         print(num)
 
-print_to_x(int(input_number))
+print_to_x(int(count_to), int(increment_by))
